bb41a211a2a1e6a6047c01918dbd3cf7e3709e53
katyazametnina-ops <katya.zametnina@mail.ru>
2026-04-03 11:10:22 +0300

    Задание №5 И тут я поняла, что я тупая.

---
+++ tasks-for_JS/hw-5/hw-5.js
@@ -0,0 +1,29 @@
+let monthNumber = 12;
+switch (monthNumber) {
+    case 12:
+    case 1:
+         case 2:
+        console.log('зима');
+        break;
+   
+        case 3:
+            case 4:
+                case 5:
+        console.log('весна');
+        break;
+        case 6:
+             case 7:
+                 case 8:
+        console.log('лето');
+        break;
+         case 9:
+            case 10:
+                case 11:
+        console.log('осень');
+        break;
+   
+
+    default:
+        console.log('месяцев всего 12')
+        break;
+}
