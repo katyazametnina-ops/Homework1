728db01861137ac1beabfceda7f51c0bac5b1c83
katyazametnina-ops <katya.zametnina@mail.ru>
2026-04-06 13:19:29 +0300

    Выполнено 5 заданий

---
+++ JS/hw-1/hw-1.js
@@ -0,0 +1,3 @@
+for (let hi = 0; hi <= 1; hi++) {
+  console.log("Привет");
+}
---
+++ JS/hw-5/hw-5.js
@@ -0,0 +1,9 @@
+for (let n = 1000; n > 50; n /= 2) {
+  if (n >= 50) {
+    console.log(n);
+  } else if(n < 50){
+    break;
+  }
+}
+alert('Конечное число - 62.5')
+let num = 4;
