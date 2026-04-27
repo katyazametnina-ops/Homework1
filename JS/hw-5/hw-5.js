21b0e9f260a4554769cabad8618205ad68605f69
katyazametnina-ops <katya.zametnina@mail.ru>
2026-04-27 18:29:12 +1000

    Исправлена задача №5

--- JS/hw-5/hw-5.js
+++ JS/hw-5/hw-5.js
@@ -1,6 +1,10 @@
 // Задание 5
 // Создать функцию, которая выводит в консоль случайное число от 1 до 10.
 
-let num = Math.random() * 11;
-
-console.log(Math.floor(num));
+function randomNumb() {
+  let num = Math.random() * 11;
+  let numRounding = Math.floor(num);
+  return numRounding;
+}
+let result = randomNumb();
+console.log(result);
