06951dbaf2fb48f65366ff4b1b2b5c128c67259f
katyazametnina-ops <katya.zametnina@mail.ru>
2026-05-01 12:19:39 +1000

    Задание №1

--- JS/hw-1/hw-1.js
+++ JS/hw-1/hw-1.js
@@ -1,8 +1,36 @@
 // Задание 1
-// Преобразовать строку 'js' в верхний регистр.
+// С помощью метода массива
+// sort
+//  отсортируйте массив
+// people
+//  по возрастанию возраста и выведите результат в консоль.
 
-// Строка 'js' преобразована в верхний регистр с помощью метода toUpperCase() и выведена.
+// const people = [
+//    { name: 'Глеб', age: 29 },
+//    { name: 'Анна', age: 17 },
+//    { name: 'Олег', age: 7 },
+//    { name: 'Оксана', age: 47 }
+// ];
 
-let string = 'js';
-let stringBig = string.toUpperCase();
-console.log(stringBig);
+// // Допишите колбэк для sort, изучите, как работает колбэк, в документации
+// console.log(people.sort(...));
+// // код выше должен вывеcти =>
+// // [
+// //  { name: 'Олег', age: 7 },
+// //  { name: 'Анна', age: 17 },
+// //  { name: 'Глеб', age: 29 },
+// //  { name: 'Оксана', age: 47 }
+// // ]
+
+const people = [
+  { name: "Глеб", age: 29 },
+  { name: "Анна", age: 17 },
+  { name: "Олег", age: 7 },
+  { name: "Оксана", age: 47 },
+];
+
+const sortedPeople = people.sort((personA, personB) => {
+  return personA.age - personB.age;
+});
+
+console.log(sortedPeople);
